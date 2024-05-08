990cca6219324f57f8e362ef9de47801762e5356
unknown <thamodyabandara4@gmail.com>
2024-05-08 11:44:06 +0530

    initial commit

---
+++ src/main/java/com/thamodhya/Springboot_Security/model/MyUserRepository.java
@@ -0,0 +1,11 @@
+package com.thamodhya.Springboot_Security.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+
+    Optional<MyUser> findByUsername(String username);
+}
+
---
+++ src/test/java/com/thamodhya/Springboot_Security/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thamodhya.Springboot_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
